542d8d316714daabf9608c0d283077b271be0f1b
xyz <gky44px1999@gmail.com>
2022-02-12 15:36:42 -0800

    gpio_test except ctrl-c

--- gpio_test.py
+++ gpio_test.py
@@ -8,10 +8,15 @@ MYPIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(MYPIN, GPIO.OUT)
 
-while True:
-    GPIO.output(MYPIN, True)
-    print("true")
-    time.sleep(8)
-    GPIO.output(MYPIN, False)
-    print("false")
-    time.sleep(2)
+
+if __name__ == '__main__':
+    try:
+        while True:
+            GPIO.output(MYPIN, True)
+            print("true")
+            time.sleep(8)
+            GPIO.output(MYPIN, False)
+            print("false")
+            time.sleep(2)
+    except KeyboardInterrupt:
+            GPIO.output(MYPIN, False)
