af2b85196b972455c769b3aab02b0b2cdb792a1c
xyz <gky44px1999@gmail.com>
2022-02-23 18:31:41 -0800

    symlink .sh also enable services

--- systemd/symlink_services.sh
+++ systemd/symlink_services.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-#sudo ln -s /home/pi/av-gpio-test.service /etc/systemd/system/
+#sudo ln -s /home/pi/av_dev/av-gpio-test.service /etc/systemd/system/
 sudo ln -s /home/pi/av_dev/systemd/av-motor.service /etc/systemd/system/
 sudo ln -s /home/pi/av_dev/systemd/av-sudo-pulseaudio.service /etc/systemd/system/
 sudo ln -s /home/pi/av_dev/systemd/av-voice.service /etc/systemd/system/
+
+#sudo systemctl enable --now av-gpio-test
+sudo systemctl enable --now av-motor
+sudo systemctl enable --now av-sudo-pulseaudio
+sudo systemctl enable --now av-voice
