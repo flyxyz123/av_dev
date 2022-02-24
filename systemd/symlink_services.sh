4b5e0046fc66090bdc9b7f146dd12239bf5cfb74
xyz <gky44px1999@gmail.com>
2022-02-23 18:32:18 -0800

    reload deamon

--- systemd/symlink_services.sh
+++ systemd/symlink_services.sh
@@ -5,6 +5,8 @@ sudo ln -s /home/pi/av_dev/systemd/av-motor.service /etc/systemd/system/
 sudo ln -s /home/pi/av_dev/systemd/av-sudo-pulseaudio.service /etc/systemd/system/
 sudo ln -s /home/pi/av_dev/systemd/av-voice.service /etc/systemd/system/
 
+sudo systemctl daemon-reload
+
 #sudo systemctl enable --now av-gpio-test
 sudo systemctl enable --now av-motor
 sudo systemctl enable --now av-sudo-pulseaudio
