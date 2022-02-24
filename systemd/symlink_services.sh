b524cdd1d13f20ae277f0e9072a7a892f3e0fb2e
xyz <gky44px1999@gmail.com>
2022-02-23 17:56:58 -0800

    refactor

--- systemd/symlink_services.sh
+++ systemd/symlink_services.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 #sudo ln -s /home/pi/av-gpio-test.service /etc/systemd/system/
-sudo ln -s /home/pi/av-motor.service /etc/systemd/system/
-sudo ln -s /home/pi/av-sudo-pulseaudio.service /etc/systemd/system/
-sudo ln -s /home/pi/av-voice.service /etc/systemd/system/
-sudo ln -s /home/pi/symlink_services.sh /etc/systemd/system/
+sudo ln -s /home/pi/av_dev/systemd/av-motor.service /etc/systemd/system/
+sudo ln -s /home/pi/av_dev/systemd/av-sudo-pulseaudio.service /etc/systemd/system/
+sudo ln -s /home/pi/av_dev/systemd/av-voice.service /etc/systemd/system/
