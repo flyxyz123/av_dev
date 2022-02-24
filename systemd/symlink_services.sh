9b7616efaf0910bd125457ec3f058ddf36856531
xyz <gky44px1999@gmail.com>
2022-02-23 17:54:06 -0800

    start voice after sudo pulseaudio

--- systemd/symlink_services.sh
+++ systemd/symlink_services.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo ln -s /home/pi/av-gpio-test.service /etc/systemd/system/
+#sudo ln -s /home/pi/av-gpio-test.service /etc/systemd/system/
 sudo ln -s /home/pi/av-motor.service /etc/systemd/system/
 sudo ln -s /home/pi/av-sudo-pulseaudio.service /etc/systemd/system/
 sudo ln -s /home/pi/av-voice.service /etc/systemd/system/
