527ff61d56af880b23fd0a16c4fc31559c15a85a
xyz <gky44px1999@gmail.com>
2022-03-19 16:04:09 -0700

    refactor

--- video_receiver/ssh_ffmpeg_mpv.sh
+++ video_receiver/ssh_ffmpeg_mpv.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video0 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --fs --title="video0" --profile=low-latency --demuxer=mkv -' &
-sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video2 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --fs --title="video2" --profile=low-latency --demuxer=mkv -' &
-sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video4 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --fs --title="video4" --profile=low-latency --demuxer=mkv -' &
+# --autofit for ubuntu mpv version 0.32.0, arch this seems to be default
+sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video0 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video0 --profile=low-latency --demuxer=mkv -' &
+sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video2 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video2 --profile=low-latency --demuxer=mkv -' &
+sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video4 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video4 --profile=low-latency --demuxer=mkv -' &
+
+# other useful options
+# mpv: --untimed
+# ffmpeg: -b:v 500k -maxrate 500k -minrate 500k
