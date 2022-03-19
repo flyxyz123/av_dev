5d24e5cb2fd0707aa6578a80322bb6aeead4faaa
xyz <gky44px1999@gmail.com>
2022-03-19 14:44:31 -0700

    bug, use /bin/sh

--- video_receiver/ssh_ffmpeg_mpv.sh
+++ video_receiver/ssh_ffmpeg_mpv.sh
@@ -1,4 +1,4 @@
-#!/bin/ssh
+#!/bin/sh
 
 sh -c 'sleep 5 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -i /dev/video0 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --title="video0" --profile=low-latency --untimed --demuxer=mkv -' &
 sh -c 'sleep 5 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -i /dev/video2 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --title="video2" --profile=low-latency --untimed --demuxer=mkv -' &
