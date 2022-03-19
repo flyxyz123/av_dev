0bc1b712f13512e0945aba47a651e7abfce42019
xyz <gky44px1999@gmail.com>
2022-03-19 15:16:40 -0700

    refactor

--- video_receiver/ssh_ffmpeg_mpv.sh
+++ video_receiver/ssh_ffmpeg_mpv.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-( sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video0 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --fs --title="video0" --profile=low-latency --untimed --demuxer=mkv - ) &
-( sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video2 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --fs --title="video2" --profile=low-latency --untimed --demuxer=mkv - ) &
-( sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video4 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --fs --title="video4" --profile=low-latency --untimed --demuxer=mkv - ) &
+sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video0 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --fs --title="video0" --profile=low-latency --untimed --demuxer=mkv -' &
+sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video2 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --fs --title="video2" --profile=low-latency --untimed --demuxer=mkv -' &
+sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video4 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --fs --title="video4" --profile=low-latency --untimed --demuxer=mkv -' &
