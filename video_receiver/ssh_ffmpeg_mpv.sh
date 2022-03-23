f30e1508b30adc8b2ff3fdeac89f90c83e09d3ae
xyz <gky44px1999@gmail.com>
2022-03-23 16:35:36 -0700

    fix multiple ffmpeg hangs when specify more than 320x240 resolution

--- video_receiver/ssh_ffmpeg_mpv.sh
+++ video_receiver/ssh_ffmpeg_mpv.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # --autofit for ubuntu mpv version 0.32.0, arch this seems to be default
-sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video0 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video0 --profile=low-latency --demuxer=mkv -' &
-sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video2 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video2 --profile=low-latency --demuxer=mkv -' &
-sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 320x240 -i /dev/video4 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video4 --profile=low-latency --demuxer=mkv -' &
+# Must specify `-c:v mjpeg` in input side. Else only one ffmpeg process is running if specify more than 320x240, all other ffmpeg processes just hang on rpi4, not sure why
+sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -c:v mjpeg -i /dev/video0 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video0 --profile=low-latency --demuxer=mkv -' &
+sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -c:v mjpeg -i /dev/video2 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video2 --profile=low-latency --demuxer=mkv -' &
+sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -c:v mjpeg -i /dev/video4 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video4 --profile=low-latency --demuxer=mkv -' &
 
 # other useful options
 # mpv: --untimed
