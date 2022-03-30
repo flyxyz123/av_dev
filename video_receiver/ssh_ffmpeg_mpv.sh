96378561af7bead65779187dfbe306993a422a29
xyz <gky44px1999@gmail.com>
2022-03-30 16:02:57 -0700

    refactor

--- video_receiver/ssh_ffmpeg_mpv.sh
+++ video_receiver/ssh_ffmpeg_mpv.sh
@@ -1,20 +1,15 @@
 #!/bin/sh
 
+# --autofit for ubuntu mpv version 0.32.0, arch this seems to be default
+# Must specify `-c:v mjpeg` in input side. Else only one ffmpeg process is running if specify more than 320x240, all other ffmpeg processes just hang on rpi4, not sure why
+# other maybe useful options:
+# mpv: --untimed
+# ffmpeg: -b:v 500k -maxrate 500k -minrate 500k
+
 display_video () {
 	sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -c:v mjpeg -i "/dev/video$1" -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title="video$1" --profile=low-latency --demuxer=mkv -
 	#sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -c:v mjpeg -i "/dev/video$1" -c:v copy -f matroska - | ffplay -
 }
-
-# --autofit for ubuntu mpv version 0.32.0, arch this seems to be default
-# Must specify `-c:v mjpeg` in input side. Else only one ffmpeg process is running if specify more than 320x240, all other ffmpeg processes just hang on rpi4, not sure why
-
-#sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -c:v mjpeg -i /dev/video0 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video0 --profile=low-latency --demuxer=mkv -' &
-#sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -c:v mjpeg -i /dev/video2 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video2 --profile=low-latency --demuxer=mkv -' &
-#sh -c 'sleep 3 && sshpass -p raspberry ssh pi@raspberrypi.local ffmpeg -an -f video4linux2 -s 1920x1080 -c:v mjpeg -i /dev/video4 -r 30 -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title=video4 --profile=low-latency --demuxer=mkv -' &
-
-# other useful options
-# mpv: --untimed
-# ffmpeg: -b:v 500k -maxrate 500k -minrate 500k
 
 for i in $(seq "${1:-3}"); do
 	sleep 1 && display_video $((0+(i-1)*2)) &
