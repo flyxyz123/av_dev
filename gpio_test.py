440d4b3de9c63e3271e0c654d998af38ade3a423
xyz <gky44px1999@gmail.com>
2022-04-22 16:32:46 -0700

    refactor

--- gpio_test.py
+++ gpio_test.py
@@ -3,20 +3,37 @@
 import time
 import RPi.GPIO as GPIO
 
-MYPIN = 17
-
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(MYPIN, GPIO.OUT)
-
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
 
 if __name__ == '__main__':
     try:
         while True:
-            GPIO.output(MYPIN, True)
+            GPIO.output(17, True)
+            GPIO.output(27, True)
+            GPIO.output(22, True)
+            GPIO.output(5, True)
+            GPIO.output(6, True)
+            GPIO.output(26, True)
             print("true")
-            time.sleep(8)
-            GPIO.output(MYPIN, False)
+            time.sleep(4)
+            GPIO.output(17, False)
+            GPIO.output(27, False)
+            GPIO.output(22, False)
+            GPIO.output(5, False)
+            GPIO.output(6, False)
+            GPIO.output(26, False)
             print("false")
-            time.sleep(2)
+            time.sleep(4)
     except KeyboardInterrupt:
-            GPIO.output(MYPIN, False)
+            GPIO.output(17, False)
+            GPIO.output(27, False)
+            GPIO.output(22, False)
+            GPIO.output(5, False)
+            GPIO.output(6, False)
+            GPIO.output(26, False)
