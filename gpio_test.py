1fb193778a710ad54bcccbd433312e11f48121a0
xyz <gky44px1999@gmail.com>
2022-04-25 15:56:44 -0700

    refactor

--- gpio_test.py
+++ gpio_test.py
@@ -3,30 +3,30 @@
 import time
 import RPi.GPIO as GPIO
 
-LAMP_PIN_ARR = [17, 27, 22, 10, 9, 11]
+PIN_ARR = [17, 27, 22, 10, 9, 11]
 
 GPIO.setmode(GPIO.BCM)
-for pin in LAMP_PIN_ARR:
+for pin in PIN_ARR:
     GPIO.setup(pin, GPIO.OUT)
 
 if __name__ == '__main__':
     try:
         while True:
             print("true")
-            for pin in LAMP_PIN_ARR:
+            for pin in PIN_ARR:
                 print("pin", pin)
                 GPIO.output(pin, True)
                 #time.sleep(1)
             time.sleep(10)
             print("false")
-            for pin in LAMP_PIN_ARR:
+            for pin in PIN_ARR:
                 print("pin", pin)
                 GPIO.output(pin, False)
                 #time.sleep(1)
             time.sleep(2)
     except KeyboardInterrupt:
             print("false")
-            for pin in LAMP_PIN_ARR:
+            for pin in PIN_ARR:
                 print("pin", pin)
                 GPIO.output(pin, False)
                 #time.sleep(1)
