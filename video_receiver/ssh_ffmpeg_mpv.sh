eb2c79116c731b40b54ba8854a73800ba4c0e08f
xyz <gky44px1999@gmail.com>
2022-05-09 02:41:55 -0700

    video receiver, consider nvidia jetson nano

--- video_receiver/ssh_ffmpeg_mpv.sh
+++ video_receiver/ssh_ffmpeg_mpv.sh
@@ -7,10 +7,10 @@
 # ffmpeg: -b:v 500k -maxrate 500k -minrate 500k
 
 display_video () {
-	sshpass -p agapeone ssh "pi@$2.local" ffmpeg -an -f video4linux2 -s "$3" -c:v mjpeg -i "/dev/video$1" -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title="video$1" --profile=low-latency --demuxer=mkv -
+	sshpass -p agapeone ssh "$2" ffmpeg -an -f video4linux2 -s "$3" -c:v mjpeg -i "/dev/video$1" -c:v libx264 -preset ultrafast -tune zerolatency -f matroska - | mpv --autofit=100%x100% --fs --title="video$1" --profile=low-latency --demuxer=mkv -
 	#sshpass -p agapeone ssh "pi@$2.local" ffmpeg -an -f video4linux2 -s "$3" -c:v mjpeg -i "/dev/video$1" -c:v copy -f matroska - | ffplay -
 }
 
 for i in $(seq "${1:-3}"); do
-	sleep 1 && display_video $((0+(i-1)*2)) "${2:-raspberrypi}" "${3:-1920x1080}" &
+	sleep 1 && display_video $((0+(i-1)*2)) "${2:-pi@raspberrypi.local}" "${3:-1920x1080}" &
 done
