be99a49bf47fc8c55e1f6f1781855f6cca78206f
xyz <gky44px1999@gmail.com>
2022-05-21 14:37:07 -0700

    symlink sh

--- prototype_1/systemd/symlink_enable_now_services.sh
+++ prototype_1/systemd/symlink_enable_now_services.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-#sudo ln -s /home/pi/av_dev/prototype_1/systemd/av-sudo-pulseaudio.service /etc/systemd/system/
-sudo ln -s /home/pi/av_dev/prototype_1/systemd/av-motor.service /etc/systemd/system/
-sudo ln -s /home/pi/av_dev/prototype_1/systemd/av-voice.service /etc/systemd/system/
-sudo ln -s /home/pi/av_dev/prototype_1/systemd/av-camera-display.service /etc/systemd/system/
+sudo ln -s /home/pi/av_dev/prototype_1/systemd/av-sudo-pulseaudio.service /etc/systemd/system/
+#sudo ln -s /home/pi/av_dev/prototype_1/systemd/av-motor.service /etc/systemd/system/
+#sudo ln -s /home/pi/av_dev/prototype_1/systemd/av-voice.service /etc/systemd/system/
+#sudo ln -s /home/pi/av_dev/prototype_1/systemd/av-camera-display.service /etc/systemd/system/
 
 sudo systemctl daemon-reload
 
-#sudo systemctl enable --now av-sudo-pulseaudio
-sudo systemctl enable --now av-motor
-sudo systemctl enable --now av-voice
-sudo systemctl enable --now av-camera-display
+sudo systemctl enable --now av-sudo-pulseaudio
+#sudo systemctl enable --now av-motor
+#sudo systemctl enable --now av-voice
+#sudo systemctl enable --now av-camera-display
